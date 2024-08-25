0aab8c93f6a035f13dcd9db7d5eec36fadcf42ca
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2024-08-26 00:18:47 +0300

    fix

--- src/page/HomePage/HomePage.tsx
+++ src/page/HomePage/HomePage.tsx
@@ -1,23 +1,10 @@
 import styles from "./homePage.module.css";
 import stylesContainer from "../../components/layout.module.css";
-import { Link, useSearchParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import sprite from "../../images/sprite.svg";
-import { FC, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "../../redux/store";
-import { registerGoogle } from "../../redux/api";
+import { FC } from "react";
 
 const HomePage: FC = () => {
-  const dispatch = useDispatch<AppDispatch>();
-  const [searchParams] = useSearchParams();
-  const token = searchParams.get("token");
-
-  useEffect(() => {
-    if (token !== null) {
-      dispatch(registerGoogle(token));
-    }
-  }, [dispatch, token]);
-
   return (
     <div className={styles.homePageSection}>
       <div className={stylesContainer.container}>
--- src/page/RegisterPage/RegisterPage.tsx
+++ src/page/RegisterPage/RegisterPage.tsx
@@ -1,10 +1,23 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import styles from "./registerPage.module.css";
 import stylesContainer from "../../components/layout.module.css";
 import { Register } from "../../components/Register/Register";
 import { NavRegisterLogin } from "../../components/NavRegisterLogin/NavRegisterLogin";
+import { useDispatch } from "react-redux";
+import { useSearchParams } from "react-router-dom";
+import { AppDispatch } from "../../redux/store";
+import { registerGoogle } from "../../redux/api";
 
 const RegisterPage: FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const [searchParams] = useSearchParams();
+  const token = searchParams.get("token");
+
+  useEffect(() => {
+    if (token !== null) {
+      dispatch(registerGoogle(token));
+    }
+  }, [dispatch, token]);
   const [activeRegister] = useState<boolean>(true);
   return (
     <div className={styles.registerPageSection}>
